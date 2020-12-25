ca756f06e6a6c348cee7b0ef50773cf38454c15c
Kelly Littlepage <kelly.littlepage@gmail.com>
2020-12-25 12:31:43 -0700

    Day 25: Combo Breaker

--- aoc/d25/main.py
+++ aoc/d25/main.py
@@ -1,4 +1,6 @@
-from typing import IO, Tuple
+import math
+
+from typing import IO, Optional, Tuple
 
 
 def parse(input_file: IO) -> Tuple[int, int]:
@@ -7,17 +9,31 @@ def parse(input_file: IO) -> Tuple[int, int]:
     return int(card), int(door)
 
 
+def baby_step_giant_step(g: int, h: int, p: int) -> Optional[int]:
+    '''
+    Solve for h = g^x mod p given a prime p.
+
+    https://en.wikipedia.org/wiki/Baby-step_giant-step
+    '''
+    N = math.ceil(math.sqrt(p - 1))
+    lookup_table = {pow(g, i, p): i for i in range(N)}
+    c = pow(g, N * (p - 2), p)
+
+    for j in range(N):
+        y = (h * pow(c, j, p)) % p
+        if y in lookup_table:
+            return j * N + lookup_table[y]
+
+    return None
+
+
 def p_1(input_file: IO,
         debug=False):  # pylint: disable=unused-argument
+    p = 20201227
     card, door = parse(input_file)
-
-    e = 0
-    n = 1
-    while n != door:
-        e += 1
-        n = 7*n % 20201227
-
-    return pow(card, e, 20201227)
+    res = baby_step_giant_step(7, door, p)
+    assert res is not None
+    return pow(card, res, p)
 
 
 def p_2(_input_file: IO,
